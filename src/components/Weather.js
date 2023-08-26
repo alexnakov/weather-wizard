2c3a262a30a9b14ea58c5e57c9c006af5c61d5c1
alexnakov <alex.nakov3@gmail.com>
2023-08-26 14:06:44 +0100

    uploaded icons

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,26 +2,14 @@ import React from 'react'
 import { allKeysAreNull } from '../utils/utils'
 
 export default function Weather(props) {
-  function one() {
-    if (allKeysAreNull(props.weather)) {
-      return <h2 style={{textAlign: 'center'}}>Please enter a city to view weather info</h2>
-    } else {
-      return viewTemplate
-    }
-  }
-  
+
   const viewTemplate = (
-    <div className='weather-container'>
-      <p>{props.weather.description}</p>
-      <p>{props.weather.temperature}°C</p>
-      <div className='sunrise-set-container'>
-        <p>Sunrise: {props.weather.sunrise}</p>
-        <p>Sunsset: {props.weather.sunset}</p>
-      </div>  
+    <div className='weather-top'>
+      
     </div>
   )
-  
+
   return (
-    one()
+    viewTemplate
   )
 }
