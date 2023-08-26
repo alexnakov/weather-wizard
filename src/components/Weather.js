30ab769f29b7ca6505b772cd36bbd3713c9a28c7
alexnakov <alex.nakov3@gmail.com>
2023-08-26 14:22:45 +0100

    splitting weather container into grid

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,8 +4,10 @@ import { allKeysAreNull } from '../utils/utils'
 export default function Weather(props) {
 
   const viewTemplate = (
-    <div className='weather-top'>
-      
+    <div className='weather-container-grid'>
+      <div className='div1'></div>
+      <div className='div2'></div>
+      <div className='div3'></div>
     </div>
   )
 
