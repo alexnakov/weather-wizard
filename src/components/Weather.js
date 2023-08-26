9cb59026dca3d8dfe1b381c01f5dd0a8c2fad87f
alexnakov <alex.nakov3@gmail.com>
2023-08-26 14:51:22 +0100

    change to react icons - slowly buildin temp

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import { allKeysAreNull } from '../utils/utils'
+import { WiDaySunny } from 'weather-icons-react'
 
 export default function Weather(props) {
 
   const viewTemplate = (
     <div className='weather-container-grid'>
-      <div className='div1'></div>
-      <div className='div2'></div>
-      <div className='div3'></div>
+      <div className='today-container'>
+        <div className='top'>
+          <WiDaySunny size='90px'/>
+          <span className='temp'>15</span>
+        </div>
+      </div>
+      <div className='graph-container'></div>
+      <div className='five-day-container'></div>
     </div>
   )
 
