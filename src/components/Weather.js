94f6a23d623027207349926f5a43919a986ab066
alexnakov <alex.nakov3@gmail.com>
2023-09-01 13:00:36 +0100

    made the icon map

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,17 +1,31 @@
 import React from 'react'
 import { allKeysAreNull } from '../utils/utils'
-import { WiDaySunny } from 'weather-icons-react'
+import { WiDaySunny, WiThunderstorm, WiRain, WiSnow, WiCloud, WiDaySprinkle, WiDayFog } from 'weather-icons-react'
 
 export default function Weather(props) {
+
+  const iconDescriptionMap = {
+    "Clear": <WiDaySunny className="w-icon"/>,
+    "Thunderstorm": <WiThunderstorm  className="w-icon"/>,
+    "Drizzle": <WiDaySprinkle  className="w-icon"/>,
+    "Rain": <WiRain  className="w-icon"/>,
+    "Snow": <WiSnow  className="w-icon"/>,
+    "Clouds": <WiCloud  className="w-icon"/>,
+    "Mist": <WiDayFog  className="w-icon"/>,
+    "Smoke": <WiDayFog  className="w-icon"/>,
+    "Haze": <WiDayFog  className="w-icon"/>,
+    "Dust": <WiDayFog  className="w-icon"/>,
+    "Fog": <WiDayFog  className="w-icon"/>
+  }
 
   const viewTemplate = (
     <div className='weather-container-grid'>
       <div className='today-container'>
         <div className='top'>
-          <WiDaySunny size='90px' className="icon"/>
+          {iconDescriptionMap[props.weather.main]}
           <div className="temp-unit-static-container">
             <div className='rel-container'>
-              <div className='temp'>15</div>
+              <div className='temp'>{props.weather.temp}</div>
               <div className='unit-container'>
                 <span>C</span>
                 <span>|</span>
@@ -21,9 +35,9 @@ export default function Weather(props) {
           </div>
         </div>
         <div className='bottom'>
-          <p className='day-stat'>Humidity: </p>
-          <p className='day-stat'>Humidity: </p>
-          <p className='day-stat'>Humidity: </p>
+          <p className='day-stat'>Cloudiness: {props.weather.cloudiness}</p>
+          <p className='day-stat'>Humidity: {props.weather.humidity}</p>
+          <p className='day-stat'>Wind: {props.weather.wind}</p>
         </div>
       </div>
       <div className='graph-container'></div>
